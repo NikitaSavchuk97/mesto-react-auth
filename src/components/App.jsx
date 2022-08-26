e93adf5c487bce9a7a9426977b604cfb7c9c86d5
Nikita Savchuk <NikitaSavchuk1997@yandex.ru>
2022-08-26 16:55:38 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,7 +35,7 @@ function App() {
 	function handleEditAvatarClick() { setIsEditAvatarPopupOpen(true) };
 	function handleEditProfileClick() { setIsEditInfoPopupOpen(true) };
 	function handleAddCardClick() { setIsAddCardPopupOpen(true) };
-	function handleConfirmClick() { setIsConfirmPopupOpen(true) };
+	//function handleConfirmClick() { setIsConfirmPopupOpen(true) };
 
 	function closeThisPopup() {
 		setIsEditAvatarPopupOpen(false);
